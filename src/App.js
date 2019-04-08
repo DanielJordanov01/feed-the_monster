9d2209aafcd75d50be61a537abaef5ca1c4f73de
Daniel Jordanov <daniel159696@gmail.com>
2019-04-08 22:16:47 +0300

    Updated the drag and drop and made the donut disappear when eaten

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import React, { Component } from "react";
 import Gabi from "./components/Gabi";
 import Donut from "./components/Donut";
-import Draggable from "react-draggable";
+import { DragDropContainer, DropTarget } from "react-drag-drop-container";
+import "tachyons";
 import "./App.css";
 
 class App extends Component {
-  render() {
-    const dragHandlers = { onStart: this.onStart, onStop: this.onStop };
-    return (
-      <div className="App">
-        <h1>Нахрани Габи!</h1>
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+  }
 
-        <Gabi />
-        <Draggable {...dragHandlers}>
-          <div className="handle">
+  onEat = e => {
+    e.containerElem.style.visibility = "hidden";
+    let counter = this.state.counter + 1;
+    this.setState({ counter });
+  };
+
+  render() {
+    return (
+      <div className="App flex flex-column">
+        <h1>Нахрани Габи!</h1>
+        <h2 className="tc">Донъти, които Габи е изяла: {this.state.counter}</h2>
+        <div className="flex justify-center justify-between mh5 mv7">
+          <DropTarget targetKey="foo" onHit={this.onEat}>
+            <Gabi />
+          </DropTarget>
+
+          <DragDropContainer targetKey="foo">
             <Donut />
-          </div>
-        </Draggable>
+          </DragDropContainer>
+          <DragDropContainer targetKey="foo">
+            <Donut />
+          </DragDropContainer>
+        </div>
       </div>
     );
   }
--- src/components/Gabi.js
+++ src/components/Gabi.js
@@ -1,13 +1,9 @@
 import React from "react";
 
-const testing = () => {
-  console.log("!!!");
-};
-
 const Gabi = () => {
   return (
     <div id="Gabi">
-      <h2 onPointerEnter={testing}>gabi</h2>
+      <h2>gabi</h2>
     </div>
   );
 };
